64550898ac7f8b55c57232a1235e9eb442307a03
Foggerty <foggerty@gmail.com>
2025-01-09 06:26:51 +1300

    Added Firefox user prefs.

---
+++ .mozilla/firefox/user.js
@@ -0,0 +1,13 @@
+user_pref("browser.download.alwaysOpenPanel", false);
+
+user_pref("full-screen-api.warning.timeout", 0);
+
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("gfx.webrender.compositor", true);
+
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", true);
+
+user_pref("webgl.force-enabled", true);
+user_pref("webgl.msaa-force", true);
