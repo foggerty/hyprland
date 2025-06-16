7f4f95dc70f5dff06bdc5f8800ca9942ca844a6d
Foggerty <foggerty@gmail.com>
2025-06-16 21:14:06 +1200

    Added Niri config, but not using for now.

--- .config/harden/harden.sh
+++ .config/harden/harden.sh
@@ -3,11 +3,11 @@
 #echo "Changing umask to 027..."
 #sudo sed -i -r 's/^UMASK.*$/UMASK\t\t022/' /etc/login.defs
 
-#echo "Diabling various modules..."
-#sudo cp ~/.config/harden/modules-blacklist.conf /etc/modprobe.d/
+echo "Diabling various modules..."
+sudo cp ~/.config/harden/modules-blacklist.conf /etc/modprobe.d/
 
 echo "Turning off core-dump in Systemd."
-if [[ ! -d /etc/systemd//coredump.conf.d ]]; then
+if [[ ! -d /etc/systemd/coredump.conf.d ]]; then
     sudo mkdir -p /etc/systemd/coredump.conf.d
 fi
 sudo tee /etc/systemd/coredump.conf.d/custom.conf <<EOF
